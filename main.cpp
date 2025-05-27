e92a2ac2bd5e64731b83ea069a8f69f7ce6fbc17
cami9553 <camilarivero1997@outlook.com>
2025-05-27 00:41:27 -0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std; 
+
+void mostrarMenu() { 
+     cout << "===========================" << endl; 
+     cout << "    JUEGO: ENFRENDADOS     " << endl; 
+     cout << "===========================" << endl;
+     cout << "1 - JUGAR" << endl; 
+     cout << "2 - ESTADISTICAS" << endl;
+     cout << "3 - CREDITOS" << endl;
+     cout << " --------------------------" << endl;
+     cout << "0 - SALIR" << endl;
+}
+
+int main() {
+    mostrarMenu();
+    return 0;
+}
